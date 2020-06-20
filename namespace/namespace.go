c20fd8d0379eed80124d8db13cba641191898cef
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-20 20:06:03 +0530

    Add Namespace Manager For RDF Triples
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

---
+++ namespace/namespace.go
@@ -0,0 +1,41 @@
+package namespace
+
+
+/*
+Provides a Namespace class.
+USAGE:
+	>>> ns, _ := namespace.New("https://spdx.org/rdf/terms")
+    >>> ns.Get("d4e2952")
+    {https://spdx.org/rdf/terms#d4e2952}
+	>>> ns.Get("Tag")
+	{https://spdx.org/rdf/terms#Tag}
+
+	// Alternative initializations of namespace without abstraction
+	>> ns, _ := namespace.Namespace{uri.URIRef{"https://spdx.org/rdf/terms"}}
+*/
+
+
+import (
+	"github.com/RishabhBhatnagar/gordf/uri"
+)
+
+
+type Namespace struct {
+	base uri.URIRef
+}
+
+
+// Provides an abstraction to create a Namespace directly from uri string.
+func New (namespace string) (ns Namespace, err error) {
+	uriref, err := uri.NewURIRef(namespace)
+	if err != nil {
+		return
+	}
+	return Namespace{ uriref }, nil
+}
+
+
+// Appends a fragment string at the end of the namespace string.
+func (ns *Namespace) Get(fragment string) uri.URIRef {
+	return ns.base.AddFragment(fragment)
+}
