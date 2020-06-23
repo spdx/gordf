225a63cf6134a88409c197bf36b4b1dbe113a7ef
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-23 17:58:02 +0530

    Reformat Code
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

--- rdfloader/parser/parser.go
+++ rdfloader/parser/parser.go
@@ -15,27 +15,22 @@ type Parser struct {
 }
 
 type Triple struct {
-	subject, predicate, object Node
+	Subject, Predicate, Object Node
 }
 
-func New() (parser *Parser) {
-	// creates a new parser object
-	return &Parser{
-		Triples:          []*Triple{},
-		schemaDefinition: map[string]uri.URIRef{},
-		blankNodeGetter:  BlankNodeGetter{-1},
-	}
-}
 
 func (parser *Parser) appendTriple(t *Triple) {
 	parser.Triples = append(parser.Triples, t)
 }
 
+// returns the schema-uri corresponding to the <schemaName:name> tag.
 func (parser *Parser) uriFromPair(schemaName string, name string) (uri.URIRef, error) {
 	if len(schemaName) == 0 {
 		// no schema name given.
 		return uri.NewURIRef(name)
 	}
+
+	// checking if the schemaName is declared in the root.
 	schemaURI, exists := parser.schemaDefinition[schemaName]
 	if !exists {
 		// schema name doesn't exist in the root block.
@@ -45,21 +40,39 @@ func (parser *Parser) uriFromPair(schemaName string, name string) (uri.URIRef, e
 }
 
 func (parser *Parser) parseAttributes(parentNode Node, attributes []xmlreader.Attribute) error {
+
+	// searching for rdf:about or rdf:id in all the attributes
+	tagUriString := ""
 	for _, attr := range attributes {
-		if !(attr.Name == "about" || attr.Name == "id") {
-			predicateURI, err := parser.uriFromPair(attr.SchemaName, attr.Name)
-			if err != nil {
-				return err
-			}
+		predicateURI, err := parser.uriFromPair(attr.SchemaName, attr.Name)
+		if err != nil { return err }
+		if predicateURI.String() == RDFNS + "about" || predicateURI.String() == RDFNS + "id" {
+			tagUriString = attr.Value
+			break
+		}
+	}
+
+	// if rdf:about was found, subject of the current tag must be the value of the rdf:about tag.
+	if len(tagUriString) > 0 {
+		tagUri, err := uri.NewURIRef(tagUriString)
+		if err != nil { return err }
+		parentNode = Node {IRI, tagUri.String()}
+	}
+
+	for _, attr := range attributes {
+		predicateURI, err := parser.uriFromPair(attr.SchemaName, attr.Name)
+		if err != nil { return err }
+		if !(predicateURI.String() == RDFNS + "about" || predicateURI.String() == RDFNS + "id") {
 			parser.appendTriple(&Triple{
-				parentNode,
-				Node{IRI, predicateURI.String()},
-				Node{IRI, attr.Value},
+				Subject:   parentNode,
+				Predicate: Node{IRI, predicateURI.String()},
+				Object:    Node{IRI, attr.Value},
 			})
 		}
 	}
 	return nil
 }
+
 
 func (parser *Parser) parseChild(parentNode Node, block *xmlreader.Block) (err error) {
 	name, schemaName := block.OpeningTag.Name, block.OpeningTag.SchemaName
@@ -71,9 +84,9 @@ func (parser *Parser) parseChild(parentNode Node, block *xmlreader.Block) (err e
 	if len(block.Children) == 0 {
 		// value attribute must be set.
 		parser.appendTriple(&Triple{
-			parentNode,
-			openingTagNode,
-			Node{LITERAL, block.Value},
+			Subject:   parentNode,
+			Predicate: openingTagNode,
+			Object:    Node{LITERAL, block.Value},
 		})
 	} else {
 		// there are children of current node.
@@ -88,24 +101,58 @@ func (parser *Parser) parseChild(parentNode Node, block *xmlreader.Block) (err e
 
 }
 
+
+func parseHeaderBlock(rootBlock xmlreader.Block) (map[string]uri.URIRef, error) {
+	// returns all the schema definitions in the root block.
+	// a schema definition is of the form xmlns:SchemaName="URI",
+
+	namespaceURI := map[string]uri.URIRef{}
+
+	for _, attr := range rootBlock.OpeningTag.Attrs {
+		if attr.SchemaName == "xmlns" {
+			uriref, err := uri.NewURIRef(attr.Value)
+			if err != nil {
+				return namespaceURI, fmt.Errorf("schema URI %v doesn't confirm to URL rules", rootBlock)
+			}
+			namespaceURI[attr.Name] = uriref
+		}
+	}
+	return namespaceURI, nil
+}
+
+
+func New() (parser *Parser) {
+	// creates a new parser object
+	return &Parser{
+		Triples:          []*Triple{},
+		schemaDefinition: map[string]uri.URIRef{},
+		blankNodeGetter:  BlankNodeGetter{-1},
+	}
+}
+
+
 func (parser *Parser) Parse(filePath string) error {
+	// reader for xml file
 	reader, err := xmlreader.XMLReaderFromFilePath(filePath)
 	if err != nil {
 		return err
 	}
-
 	rootBlock, err := reader.Read()
 	if err != nil {
 		return err
 	}
 
+	// set all the schema definitions in the root block.
 	schemaDefinition, err := parseHeaderBlock(rootBlock)
 	if err != nil {
 		return err
 	}
 	parser.schemaDefinition = schemaDefinition
 
+	// root node is always identified by a blank node.
 	rootNode := parser.blankNodeGetter.Get()
+
+	// parse each child of the root block.
 	for _, childBlock := range rootBlock.Children {
 		err = parser.parseChild(rootNode, childBlock)
 		if err != nil {
@@ -113,19 +160,4 @@ func (parser *Parser) Parse(filePath string) error {
 		}
 	}
 	return nil
-}
-
-func parseHeaderBlock(rootBlock xmlreader.Block) (map[string]uri.URIRef, error) {
-	namespaceURI := map[string]uri.URIRef{}
-	for _, attr := range rootBlock.OpeningTag.Attrs {
-		if attr.SchemaName == "xmlns" {
-			uriref, err := uri.NewURIRef(attr.Value)
-			if err != nil {
-				err = fmt.Errorf("schema URI %v doesn't confirm to URL rules", rootBlock)
-				return namespaceURI, err
-			}
-			namespaceURI[attr.Name] = uriref
-		}
-	}
-	return namespaceURI, nil
 }
