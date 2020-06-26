fae8ed65b03019eb66df89a45500bef117366693
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-26 19:12:12 +0530

    EOD commit
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

--- rdfloader/parser/parser.go
+++ rdfloader/parser/parser.go
@@ -13,99 +13,12 @@ type Parser struct {
 	Triples          []*Triple
 	schemaDefinition map[string]uri.URIRef
 	blankNodeGetter  BlankNodeGetter
+	rdfNS uri.URIRef
 	wg sync.WaitGroup
 }
 
 type Triple struct {
-	Subject, Predicate, Object Node
-}
-
-
-func (parser *Parser) appendTriple(t *Triple) {
-	parser.Triples = append(parser.Triples, t)
-}
-
-// returns the schema-uri corresponding to the <schemaName:name> tag.
-func (parser *Parser) uriFromPair(schemaName string, name string) (uri.URIRef, error) {
-	if len(schemaName) == 0 {
-		// no schema name given.
-		return uri.NewURIRef(name)
-	}
-
-	// checking if the schemaName is declared in the root.
-	schemaURI, exists := parser.schemaDefinition[schemaName]
-	if !exists {
-		// schema name doesn't exist in the root block.
-		return uri.URIRef{}, fmt.Errorf("undefined schema name: %v", schemaName)
-	}
-	return schemaURI.AddFragment(name), nil
-}
-
-func (parser *Parser) parseAttributes(parentNode Node, attributes []xmlreader.Attribute) error {
-
-	// searching for rdf:about or rdf:id in all the attributes
-	tagUriString := ""
-	for _, attr := range attributes {
-		predicateURI, err := parser.uriFromPair(attr.SchemaName, attr.Name)
-		if err != nil { return err }
-		if predicateURI.String() == RDFNS + "about" || predicateURI.String() == RDFNS + "id" {
-			tagUriString = attr.Value
-			break
-		}
-	}
-
-	// if rdf:about was found, subject of the current tag must be the value of the rdf:about tag.
-	if len(tagUriString) > 0 {
-		tagUri, err := uri.NewURIRef(tagUriString)
-		if err != nil { return err }
-		parentNode = Node {IRI, tagUri.String()}
-	}
-
-	for _, attr := range attributes {
-		predicateURI, err := parser.uriFromPair(attr.SchemaName, attr.Name)
-		if err != nil { return err }
-		if !(predicateURI.String() == RDFNS + "about" || predicateURI.String() == RDFNS + "id") {
-			parser.appendTriple(&Triple{
-				Subject:   parentNode,
-				Predicate: Node{IRI, predicateURI.String()},
-				Object:    Node{IRI, attr.Value},
-			})
-		}
-	}
-	return nil
-}
-
-
-func (parser *Parser) parseChild(parentNode Node, block *xmlreader.Block, errp *error)  {
-	// errp: pointer to the error
-	name, schemaName := block.OpeningTag.Name, block.OpeningTag.SchemaName
-	openingTagURI, err := parser.uriFromPair(schemaName, name)
-	if err != nil {
-		*errp = err
-		return
-	}
-	openingTagNode := Node{IRI, openingTagURI.String()}
-	if len(block.Children) == 0 {
-		// value attribute must be set.
-		parser.appendTriple(&Triple{
-			Subject:   parentNode,
-			Predicate: openingTagNode,
-			Object:    Node{LITERAL, block.Value},
-		})
-	} else {
-		// there are children of current node.
-		for _, childBlock := range block.Children {
-			parser.wg.Add(1)
-			var newError error
-			parser.parseChild(openingTagNode, childBlock, &newError)
-			if newError!= nil {
-				*errp = newError
-				return
-			}
-		}
-	}
-	*errp = parser.parseAttributes(openingTagNode, block.OpeningTag.Attrs)
-	parser.wg.Done()
+	Subject, Predicate, Object *Node
 }
 
 
@@ -128,16 +41,95 @@ func parseHeaderBlock(rootBlock xmlreader.Block) (map[string]uri.URIRef, error) 
 }
 
 
+func (parser *Parser) uriFromPair(name, schemaName string) (mergedUri uri.URIRef, err error) {
+	// returns the uri representation of a pair of strings.
+	// name:schemaName is an example of pair.
+	// pairs such as rdf:RDF, where, rdf must be a valid xmlns schema name.
+
+	// base must be a valid schema name defined in the root tag.
+	baseURI, ok := parser.schemaDefinition[name]
+	if !ok {
+		return uri.URIRef{}, fmt.Errorf("undefined schema name: %v", name)
+	}
+
+	// adding the relative fragment to the base uri.
+	return baseURI.AddFragment(schemaName), nil
+}
+
+
+func (parser *Parser) nodeFromTag(openingTag xmlreader.Tag) (node *Node, err error) {
+	// returns the node object from the opening tag of any block.
+	// https://www.w3.org/TR/rdf-syntax-grammar/figure1.png has sample image having 5 nodes.
+	// 		one of them is a blank node.
+
+	// description of the entire function:
+	// if the opening tag has an attribute of rdf:about,
+	//		the node will represented by the value of rdf:about attribute
+	// else, it is a blank node.
+
+	// checking if any of the attributes is a rdf:about attribute
+	index := -1
+	for i, attr := range openingTag.Attrs {
+		attrUri, err := parser.uriFromPair(attr.SchemaName, attr.Name)
+		if err != nil { return }
+		if attrUri == parser.rdfNS.AddFragment("about") {
+			// current attribute is a rdf:about tag,
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		// we didnt' find rdf:about in the attributes of the opening tag.
+		// returnning a new blank node.
+		blankNode := parser.blankNodeGetter.Get()
+		return &blankNode, nil
+	}
+
+	// we found a rdf:about tag.
+	currentNode := Node{
+		NodeType: IRI,
+		Val: openingTag.Attrs[index].Value,
+	}
+	return &currentNode, nil
+}
+
+
 func New() (parser *Parser) {
 	// creates a new parser object
+	rdfNS, _ := uri.NewURIRef(RDFNS)
 	return &Parser{
 		Triples:          []*Triple{},
-		schemaDefinition: map[string]uri.URIRef{},
+		schemaDefinition: map[string]uri.URIRef{"": uri.URIRef{}},
 		blankNodeGetter:  BlankNodeGetter{-1},
 		wg: sync.WaitGroup{},
+		rdfNS: rdfNS,
 	}
 }
 
+
+func (parser *Parser) parseBlock(block *xmlreader.Block) (err error) {
+	/*
+	1. What is a block?
+	Ans: A rdf block is made up of
+			1. Root Node (IRI Ref or BlankNode) :: Subject
+			2. Link (IRI Ref)                   :: Object
+			3. anotherBlock (Literal or IRI Ref or Blank Node) :: Predicate
+
+	2. Example of a Block.
+		Sample RDF/XML input with non-blank subject and literal predicate.
+			<spdx:License rdf:about="http://spdx.org/licenses/Apache-2.0">
+				<spdx:licenseId>Apache-2.0</spdx:licenseId>
+			</spdx:License>
+		Output Components:
+			Subject:   http://spdx.org/licenses/Apache-2.0  (IRI Ref)
+			Object:    spdx:licenseId						(IRI Ref)
+			Predicate: Apacha-2.0							(Literal)
+
+		If the rdf:about attribute of the subject is removed, it will become a blank node.
+	*/
+
+}
 
 func (parser *Parser) Parse(filePath string) error {
 	// reader for xml file
@@ -145,6 +137,7 @@ func (parser *Parser) Parse(filePath string) error {
 	if err != nil {
 		return err
 	}
+	// parsing the xml file
 	rootBlock, err := reader.Read()
 	if err != nil {
 		return err
@@ -157,17 +150,11 @@ func (parser *Parser) Parse(filePath string) error {
 	}
 	parser.schemaDefinition = schemaDefinition
 
-	// root node is always identified by a blank node.
-	rootNode := parser.blankNodeGetter.Get()
-
-	// parse each child of the root block.
-	for _, childBlock := range rootBlock.Children {
-		parser.wg.Add(1)
-		go parser.parseChild(rootNode, childBlock, &err)
-		if err != nil {
-			return err
-		}
+	// root tag is set now.
+	for _, child := range rootBlock.Children {
+		err = parser.parseBlock(nil, nil, child)
+		if err != nil { return err }
 	}
-	parser.wg.Wait()
+
 	return nil
 }
