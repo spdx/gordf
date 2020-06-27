c30fc12654005811eb3b4613a1dd7e54f0ca8d8a
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-27 15:35:19 +0530

    Fix Node Test
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

--- rdfloader/parser/node_test.go
+++ rdfloader/parser/node_test.go
@@ -10,16 +10,16 @@ func TestBlankNodeGetter_Get(t *testing.T) {
 
 	// by default, node id starts with N1.
 	firstBlankNode := blankNodeGetter.Get()
-	if firstBlankNode.Val != "N1" {
-		t.Errorf("expected first node to be N1, found %v", firstBlankNode.Val)
+	if firstBlankNode.ID != "N1" {
+		t.Errorf("expected first node to be N1, found %v", firstBlankNode.ID)
 	}
 	if nodeType := firstBlankNode.NodeType; nodeType != BLANK {
 		t.Errorf("blank node must be of type %v. Found %v", BLANK, nodeType)
 	}
 
 	secondBlankNode := blankNodeGetter.Get()
-	if secondBlankNode.Val != "N2" {
-		t.Errorf("expected node to be N2, found %v", secondBlankNode.Val)
+	if secondBlankNode.ID != "N2" {
+		t.Errorf("expected node to be N2, found %v", secondBlankNode.ID)
 	}
 
 	// blank node getter with custom lastid.
@@ -28,7 +28,7 @@ func TestBlankNodeGetter_Get(t *testing.T) {
 	}
 	// last id -1 means that first node should start from N0
 	firstBlankNode = blankNodeGetter.Get()
-	if firstBlankNode.Val != "N0" {
-		t.Errorf("Expected node to be %v, found %v", "N0", firstBlankNode.Val)
+	if firstBlankNode.ID != "N0" {
+		t.Errorf("Expected node to be %v, found %v", "N0", firstBlankNode.ID)
 	}
 }
