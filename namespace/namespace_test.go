c26b4a7793792793d2e11a625631f4a9ae92d3ad
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-06-27 16:50:18 +0530

    Add Namespace Test
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

---
+++ namespace/namespace_test.go
@@ -0,0 +1,29 @@
+package namespace
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	sampleURI := "https://www.spdx.org/rdf/terms"
+	_, err := New(sampleURI)
+	if err != nil {
+		t.Errorf("error parsing a valid URI")
+	}
+}
+
+
+func TestNamespace_Get(t *testing.T) {
+	sampleURI := "https://www.spdx.org/rdf/terms"
+	sampleNS, err := New(sampleURI)
+	if err != nil {
+		t.Errorf("error parsing a valid URI")
+		return
+	}
+	fragment := "name"
+	indexedURI := sampleNS.Get(fragment)
+	if indexedURI.String() != sampleURI + "#" + fragment {
+		t.Errorf("error adding fragment to the base URI")
+	}
+}
+
