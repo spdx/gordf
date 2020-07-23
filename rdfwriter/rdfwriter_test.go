8bcde7cf7b48e4a5e87dcbc4301fa9d655c6b924
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-07-23 20:17:38 +0530

    Write More Tests To Cover More Edge Cases
    Signed-off-by: RishabhBhatnagar <bhatnagarrishabh4@gmail.com>

--- rdfwriter/rdfwriter_test.go
+++ rdfwriter/rdfwriter_test.go
@@ -25,7 +25,7 @@ func getSampleSchemaDefinition() map[string]uri.URIRef {
 
 func TestFilterTriples(t *testing.T) {
 	nodes := getNBlankNodes(10)
-	triples := []*parser.Triple {
+	triples := []*parser.Triple{
 		{nodes[0], nodes[1], nodes[2]},
 		{nodes[3], nodes[1], nodes[4]},
 		{nodes[3], nodes[5], nodes[4]},
@@ -72,8 +72,8 @@ func TestTriplesToString(t *testing.T) {
 	// init all required variables for the testing.
 	var triples []*parser.Triple
 	schemaDefinition := getSampleSchemaDefinition()
-	tab := "    "    // 4 spaces as a tab character
-	bnodes := getNBlankNodes(3)
+	tab := "    " // 4 spaces as a tab character
+	bnodes := getNBlankNodes(5)
 
 	// TestCase 1: error raised by stringify must be returned by the function too.
 	// stringify will complain that every subject node must be associated with
@@ -88,16 +88,35 @@ func TestTriplesToString(t *testing.T) {
 		t.Errorf("expected an error stating invalid triples")
 	}
 
-
 	// TestCase 2: valid input without any triples must return just the root tag with the schemaDefinition.
 	// clearing the schemaDefinition such that the function returns just the root tags.
-	triples = nil  // clearing the slice.
+	triples = nil // clearing the slice.
 	schemaDefinition = nil
 	output, err := TriplesToString(triples, schemaDefinition, tab)
 	expectedOutput := `<rdf:RDF>
 </rdf:RDF>`
-    if output != expectedOutput {
-    	t.Errorf("expected output: %s, got output: %s", expectedOutput, output)
+	if output != expectedOutput {
+		t.Errorf("expected output: %s, got output: %s", expectedOutput, output)
+	}
+
+	// TestCase 3: simple testcase with more than one triples.
+	schemaDefinition = getSampleSchemaDefinition()
+	spdxRef := schemaDefinition["spdx"]
+	triples = []*parser.Triple{
+		{
+			Subject:   bnodes[0],
+			Predicate: &parser.Node{NodeType: parser.IRI, ID: parser.RDFNS + "type"},
+			Object:    &parser.Node{NodeType: parser.IRI, ID: spdxRef.String() + "Snippet"},
+		},
+		{
+			Subject:   bnodes[0],
+			Predicate: &parser.Node{NodeType: parser.IRI, ID: spdxRef.String() + "randomFragment"},
+			Object:    &parser.Node{NodeType: parser.LITERAL, ID: "sample"},
+		},
+	}
+	output, err = TriplesToString(triples, schemaDefinition, tab)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
 	}
 }
 
@@ -105,7 +124,7 @@ func TestWriteToFile(t *testing.T) {
 	// init all required variables for the testing.
 	var triples []*parser.Triple
 	schemaDefinition := getSampleSchemaDefinition()
-	tab := "    "    // 4 spaces as a tab character
+	tab := "    " // 4 spaces as a tab character
 	bnodes := getNBlankNodes(10)
 
 	// TestCase 1: checking if the function returns an error when the
@@ -122,7 +141,6 @@ func TestWriteToFile(t *testing.T) {
 	if err == nil {
 		t.Errorf("expected an error stating invalid triples")
 	}
-
 
 	// TestCase 2: valid case with no triples and no schema definitions in the root tag.
 	triples = nil
@@ -143,9 +161,10 @@ func Test_stringify(t *testing.T) {
 	bnodes := getNBlankNodes(10)
 	var triples []*parser.Triple
 	_, nodeToTriples := getAdjacencyList(triples)
-	invSchemaDefinition := invertSchemaDefinition(getSampleSchemaDefinition())
+	schemaDefinition := getSampleSchemaDefinition()
+	invSchemaDefinition := invertSchemaDefinition(schemaDefinition)
 	depth := 0
-	tab := "  "   // 2 spaces as the tabs
+	tab := "  " // 2 spaces as the tabs
 
 	// TestCase 1: Invalid case with no triples belonging to the passed node.
 	// expected error:  every subject node must be associated with exactly 1
@@ -173,8 +192,113 @@ func Test_stringify(t *testing.T) {
 	expectedOutput := `<spdx:Snippet>
 
 </spdx:Snippet>`
-    if output != expectedOutput {
-    	t.Errorf("output is not correct. expected output is %s. found %s", expectedOutput, output)
+	if output != expectedOutput {
+		t.Errorf("output is not correct. expected output is %s. found %s", expectedOutput, output)
+	}
+
+	// TestCase 4: invalid input with two triples: rdf:type and second invalid triples
+	triples = append(triples, &parser.Triple{
+		Subject:   bnodes[0],
+		Predicate: &parser.Node{NodeType: parser.IRI, ID: ""},
+		Object:    &parser.Node{NodeType: parser.LITERAL, ID: "comment"},
+	})
+	_, nodeToTriples = getAdjacencyList(triples)
+	_, err = stringify(bnodes[0], nodeToTriples, invSchemaDefinition, depth, tab)
+	if err == nil {
+		t.Errorf("expected an error saying invalid predicate uri")
+	}
+
+	// TestCase 4: input with two triples: rdf:type and rdf:resource
+	triples[1].Predicate.ID = "http://spdx.org/rdf/terms#algorithm"
+	triples[1].Object = &parser.Node{
+		NodeType: parser.RESOURCELITERAL,
+		ID:       "http://spdx.org/rdf/terms#checksumAlgorithm_sha256",
+	}
+	_, nodeToTriples = getAdjacencyList(triples)
+	output, _ = stringify(bnodes[0], nodeToTriples, invSchemaDefinition, depth, tab)
+	expectedOutput = `<spdx:Snippet>
+  <spdx:algorithm rdf:resource="http://spdx.org/rdf/terms#checksumAlgorithm_sha256"/>
+</spdx:Snippet>`
+	if output != expectedOutput {
+		t.Errorf("mismatching outputs. Expected:\n%v\n Found: \n%v", expectedOutput, output)
+	}
+
+	// TestCase 5: input with two triples: rdf:type and rdf:comment and second triple of object-type literal
+	triples[1] = &parser.Triple{
+		Subject: bnodes[0],
+		Predicate: &parser.Node{
+			NodeType: parser.IRI,
+			ID:       parser.RDFNS + "comment",
+		},
+		Object: &parser.Node{
+			NodeType: parser.LITERAL,
+			ID:       "comment",
+		},
+	}
+	_, nodeToTriples = getAdjacencyList(triples)
+	_, err = stringify(bnodes[0], nodeToTriples, invSchemaDefinition, depth, tab)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// TestCase 6: input with 3 level deep nested triples.
+	spdxRef := schemaDefinition["spdx"]
+	triples = []*parser.Triple{
+		{ // N1 rdf:type spdx:externalRef
+			Subject: bnodes[0],
+			Predicate: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       parser.RDFNS + "type",
+			},
+			Object: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       spdxRef.String() + "externalRef",
+			},
+		},
+		{ // N1 spdx:ExternalRef N2
+			Subject: bnodes[0],
+			Predicate: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       spdxRef.String() + "ExternalRef",
+			},
+			Object: bnodes[1],
+		},
+		{ // N2 rdf:type spdx:referenceType
+			Subject: bnodes[1],
+			Predicate: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       parser.RDFNS + "type",
+			},
+			Object: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       spdxRef.String() + "referenceType",
+			},
+		},
+		{ // N2 spdx:ReferenceType "http://spdx.org/rdf/references/cpe23Type"
+			Subject: bnodes[1],
+			Predicate: &parser.Node{
+				NodeType: parser.IRI,
+				ID:       spdxRef.String() + "ReferenceType",
+			},
+			Object: &parser.Node{
+				NodeType: parser.LITERAL,
+				ID:       "http://spdx.org/rdf/references/cpe23Type",
+			},
+		},
+	}
+	_, nodeToTriples = getAdjacencyList(triples)
+	output, _ = stringify(bnodes[0], nodeToTriples, invSchemaDefinition, depth, tab)
+	expectedOutput = `<spdx:externalRef>
+  <spdx:ExternalRef>
+    <spdx:referenceType>
+      <spdx:ReferenceType>
+        http://spdx.org/rdf/references/cpe23Type
+      </spdx:ReferenceType>
+    </spdx:referenceType>
+  </spdx:ExternalRef>
+</spdx:externalRef>`
+	if output != expectedOutput {
+		t.Errorf("mismatching outputs. Expected:\n%v\n Found: \n%v", expectedOutput, output)
 	}
 }
 
@@ -182,12 +306,11 @@ func Test_getOpeningAndClosingTags(t *testing.T) {
 	nodes := getNBlankNodes(10)
 	tab := ""
 	rdfNSAbbrev := "rdf"
-	invSchemaDefinition := map[string]string {
+	invSchemaDefinition := map[string]string{
 		"http://www.w3.org/1999/02/22-rdf-syntax-ns": "rdf",
-		"http://spdx.org/rdf/terms": "spdx",
+		"http://spdx.org/rdf/terms":                  "spdx",
 	}
 	var triples []*parser.Triple
-
 
 	// TestCase 1: empty triple list must return an error
 	_, _, err := getOpeningAndClosingTags(triples, rdfNSAbbrev, invSchemaDefinition, tab, nodes[0])
@@ -230,9 +353,9 @@ func Test_getOpeningAndClosingTags(t *testing.T) {
 
 	// TestCase 4: exactly one nodeID attribute (a valid case)
 	triples = append(triples, &parser.Triple{
-		Subject: nodes[0],
+		Subject:   nodes[0],
 		Predicate: &parser.Node{NodeType: parser.IRI, ID: parser.RDFNS + "nodeID"},
-		Object: &parser.Node{parser.LITERAL, "Node34"},
+		Object:    &parser.Node{parser.LITERAL, "Node34"},
 	})
 	openingTag, closingTag, err = getOpeningAndClosingTags(triples, rdfNSAbbrev, invSchemaDefinition, tab, nodes[0])
 	expectedOpeningTag, expectedClosingTag = `<spdx:Snippet rdf:nodeID="Node34">`, "</spdx:Snippet>"
@@ -264,6 +387,21 @@ func Test_getOpeningAndClosingTags(t *testing.T) {
 	if err == nil {
 		t.Error("expected an invalid uri error")
 	}
+
+	// TestCase 7: Valid case where we have a rdf:about tag
+	triples = []*parser.Triple{
+		{ // rdf:type="http://spdx.org/rdf/terms#Snippet"
+			Subject:   nodes[0],
+			Predicate: &parser.Node{NodeType: parser.IRI, ID: parser.RDFNS + "type"},
+			Object:    &parser.Node{NodeType: parser.IRI, ID: "http://spdx.org/rdf/terms#Snippet"},
+		},
+		{ // rdf:about="http://spdx.org/rdf/terms#Snippet132"
+			Subject:   nodes[0],
+			Predicate: &parser.Node{NodeType: parser.IRI, ID: parser.RDFNS + "type"},
+			Object:    &parser.Node{NodeType: parser.IRI, ID: "http://spdx.org/rdf/terms#Snippet132"},
+		},
+	}
+	_, _, err = getOpeningAndClosingTags(triples, rdfNSAbbrev, invSchemaDefinition, tab, nodes[0])
 }
 
 func Test_getRootTagFromSchemaDefinition(t *testing.T) {
