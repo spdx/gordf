8e66977c0c32f3de0c174ef132d29ea87c5233b1
Ram <devops.assignment.ram@gmail.com>
2024-09-06 14:07:18 +0000

    Add sample usage in the main.go file
    Signed-off-by: Ram <devops.assignment.ram@gmail.com>

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package goRdf
 
+import "fmt"
+
 func main() {
-  // Add a blank main function
+	fmt.Println("Sample Usage:")
+	fmt.Println(">> go build .")
+	fmt.Println(">> gordf <source_rdf_file>")
+	fmt.Println("This will print all the triples in the rdf files")
 }
